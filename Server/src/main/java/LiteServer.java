d5f09bc36c99f0d5ace90ae4d3ccad3c9b4765cd
d.saraev <d.saraev@a1qa.com>
2021-11-16 14:59:04 +0300

    server

--- Server/src/main/java/LiteServer.java
+++ Server/src/main/java/LiteServer.java
@@ -1,10 +1,46 @@
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
 public class LiteServer {
 
     public static void main(String[] args) {
-        new LiteServer().run();
+        try {
+            new LiteServer().run();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void run() {
+    public void run() throws InterruptedException {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        NioEventLoopGroup workersGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap()
+                    .group(bossGroup, workersGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                        @Override
+                        protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
+                            nioSocketChannel.pipeline().addLast(
+                                    new StringDecoder(),
+                                    new StringEncoder(),
+                                    new LiteServerChannelInboundHandler()
+                            );
+                        }
+                    })
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
 
+            Channel channel = serverBootstrap.bind(9000).sync().channel();
+            channel.closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workersGroup.shutdownGracefully();
+        }
     }
 }
