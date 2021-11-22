891dbacbdc21fc791e54bfcad28277306fd4f475
d.saraev <black8.tabls@gmail.com>
2021-11-23 00:13:27 +0300

    Time server with Serializable Object Message

---
+++ AlfaStorageClient/src/main/java/lite/ClientRunner.java
@@ -0,0 +1,64 @@
+package lite;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import io.netty.handler.codec.LengthFieldPrepender;
+import io.netty.handler.codec.serialization.ClassResolvers;
+import io.netty.handler.codec.serialization.ObjectDecoder;
+import io.netty.handler.codec.serialization.ObjectEncoder;
+
+import java.util.Date;
+
+public class ClientRunner {
+
+    public static void main(String[] args) throws InterruptedException {
+        new ClientRunner().start();
+    }
+
+    public void start() {
+        NioEventLoopGroup worker = new NioEventLoopGroup(1);
+        try {
+            Bootstrap bootstrap = new Bootstrap()
+                    .group(worker)
+                    .channel(NioSocketChannel.class)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) {
+                            ch.pipeline().addLast(
+                                    new LengthFieldBasedFrameDecoder(1024 * 1024, 0, 3, 0, 3),
+                                    new LengthFieldPrepender(3),
+                                    new ObjectEncoder(),
+                                    new ObjectDecoder(ClassResolvers.cacheDisabled(null)),
+                                    new SimpleChannelInboundHandler<Message>() {
+                                        @Override
+                                        protected void channelRead0(ChannelHandlerContext ctx, Message msg) {
+                                            System.out.println("Входящее сообщение: " + msg.getText());
+                                        }
+                                    }
+                            );
+                        }
+                    })
+                    .option(ChannelOption.SO_KEEPALIVE, true);
+
+            System.out.println("Клтент запущен...");
+
+            Channel channel = bootstrap.connect("localhost", 9000).sync().channel();
+
+            while (true) {
+                Message message = new Message();
+                message.setText("New message form client: " + new Date());
+                channel.writeAndFlush(message);
+                Thread.sleep(5000);
+            }
+
+        } catch (InterruptedException e) {
+            System.out.println("Клиент прервал свою работу...");
+        } finally {
+            worker.shutdownGracefully();
+        }
+    }
+}
