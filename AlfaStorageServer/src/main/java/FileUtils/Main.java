d57c209023eb7ef5b89edda6d6e7093d73b416ff
d.saraev <d.saraev@a1qa.com>
2021-11-30 19:15:40 +0300

    files

--- AlfaStorageServer/src/main/java/FileUtils/Main.java
+++ AlfaStorageServer/src/main/java/FileUtils/Main.java
@@ -1,13 +1,24 @@
 package FileUtils;
 
 import java.io.File;
-import java.util.Properties;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) {
-        String path = System.getProperty("user.dir") + "\\AlfaStorageServer\\src\\main\\resources\\";
-        System.out.println(path);
-        File file = new File(path + "f.txt");
+
+        try {
+            String path = System.getProperty("user.dir") + "\\AlfaStorageServer\\src\\main\\resources\\";
+            File newFilePath = new File(path + "file.txt");
+            newFilePath.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.setProperty("Super", "Man");
+        MainTwo.print();
 
     }
 }
---
+++ AlfaStorageServer/src/main/java/FileUtils/MainTwo.java
@@ -0,0 +1,12 @@
+package FileUtils;
+
+public class MainTwo {
+
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("Super"));
+    }
+
+    public static void print() {
+        System.out.println(System.getProperty("Super"));
+    }
+}
